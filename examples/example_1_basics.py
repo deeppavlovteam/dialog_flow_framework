9a3face630d1951402d9f4841192ae9cfa8d3b33
Denis Kuznetosv <kuznetsov.den.p@gmail.com>
2021-09-16 16:19:36 +0300

    gui/example: add example

--- examples/example_1_basics.py
+++ examples/example_1_basics.py
@@ -31,44 +31,23 @@ def preproc4():pass
 def preproc5():pass
 
 
-GLOBAL = 1
-LOCAL = 1
-PROCESSING = 1
-PRE_RESPONSE = 1
-POST_RESPONSE = 1
 
 flows = {
     GLOBAL: {
-        TRANSITIONS: {},
-        PROCESSING: {1: [preproc1], 2: [preproc2], 3: [preproc3]},
+        TRANSITIONS: {"start_node":cnd.exact_match("Hi")},
+        PROCESSING: {},
         RESPONSE: {},
     },
     "greeting_flow": {
         LOCAL: {
             TRANSITIONS: {},
             PROCESSING: {2: [preproc4]},
-            RESPONSE: {},
+            RESPONSE: "Hi",
         },
         "start_node": {  # This is an initial node, it doesn't need an `RESPONSE`
             RESPONSE: "",
             TRANSITIONS: {"node1": cnd.exact_match("Hi")},  # If "Hi" == request of user then we make the transition
             PROCESSING: {3: [preproc5]},
-        },
-        "node1": {
-            RESPONSE: "Hi, how are you?",  # When the agent goes to node1, we return "Hi, how are you?"
-            TRANSITIONS: {"node2": cnd.exact_match("i'm fine, how are you?")},
-        },
-        "node2": {
-            RESPONSE: "Good. What do you want to talk about?",
-            TRANSITIONS: {"node3": cnd.exact_match("Let's talk about music.")},
-        },
-        "node3": {
-            RESPONSE: "Sorry, I can not talk about music now.",
-            TRANSITIONS: {"node4": cnd.exact_match("Ok, goodbye.")},
-        },
-        "node4": {
-            RESPONSE: "bye",
-            TRANSITIONS: {"node1": cnd.exact_match("Hi")},
         },
         "fallback_node": {  # We get to this node if an error occurred while the agent was running
             RESPONSE: "Ooops",
